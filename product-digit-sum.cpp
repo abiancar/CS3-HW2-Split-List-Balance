c98186cf73f725207708f6338baaade6e37ce5f4
Agustin Biancardi <agus@pop-os.localdomain>
2021-01-06 15:10:53 -0500

    Added sum() function to add digits of power result.

--- product-digit-sum.cpp
+++ product-digit-sum.cpp
@@ -1,38 +1,46 @@
 #include <iostream>
 #include <limits.h>
+#include <string>
 using namespace std; 
 
+//OTHER CHANGES: Make variable names more relevant : "result" and "sum" should be made into two words if possible
 
+long long pow(int base,int power){
+    long long totalResult = 1;
 
-void pow(int base,int power){
-
-    long long totalResult = 1; 
     for (int i = 0; i < power; i++){
         totalResult = totalResult * base;
     }
-    cout << base <<" raised to the power of " << power << " = " << totalResult << endl; 
+    cout << base <<"^ " << power << "= " << totalResult << endl; 
+    return totalResult;
 }
 
+void sum(long long result){
+    string rString = to_string(result);
+    int sum = 0;
+    for (unsigned int i = 0; i < rString.size(); i++ ){
+        int curr = int(rString.at(i)) -48; // it felt awkward converting from long long to string and then turning a number character to an int... 
+        sum += curr;                       // let's see if it can be simplified
+    }
+    cout << "Sum of Digits: " << sum;
+}
+
+int main() {    
+    int base = -1;  //FIX ME: Let's solve this problem using signed integers when I have more time [just to make things a bit more sophisticated]
+    int power = -1; // Get rid of while loop, try and catch if there are any errors, repeat prompt
 
 
-
-
-
-
-int main() {
-    
-    int base = -1;
-    int power = -1;
-
+    //ensure userInputs area positive integers
     while (base < 0){
         cout << "What is the base? (Must be a positive integer!) : ";       
         cin >> base;
     }
-    
 
-    cout << "What is the power?: ";
-    cin >> power; 
-
-    pow(base, power);     
+    while (power < 0){
+        cout << "What is the power? (Must be a positive integer!) : ";       
+        cin >> power;
+    }
+    cout << "base: " << base << endl << "power: " << power << endl;
+    sum(pow(base, power));   //FIX ME: Code runs fine, but let's improve readability by separating sum() and pow()
     return 0; 
 }
